a8fba34ce74b6b87e8bdc7bfeba9f2b20553ece4
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-11 17:59:16 +0300

    layout for providing context values

---
+++ frontend/src/Layout.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { themeContext } from "./Contexts/themeContext";
+import { languageContext, type Languages } from "./Contexts/languageContext";
+
+type ThemeType = "light" | "dark" | "colorful";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<ThemeType>("light");
+  const [language, setLanguage] = useState<Languages>("eng");
+
+  useEffect(() => {
+    const chosenLang = localStorage.getItem("lang");
+    if (chosenLang) setLanguage(chosenLang as Languages);
+    const chosenTheme = localStorage.getItem("theme");
+    if (chosenTheme) setTheme(chosenTheme as ThemeType);
+  }, []);
+
+  return (
+    <themeContext.Provider value={{ theme, setTheme }}>
+      <languageContext.Provider value={{ language, setLanguage }}>
+        {children}
+      </languageContext.Provider>
+    </themeContext.Provider>
+  );
+};
+
+export default Layout;
