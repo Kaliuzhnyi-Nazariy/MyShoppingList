68e7c61b1e504cdfdc371cde5a9ad09b4a499b6b
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-11 21:50:26 +0300

    message and button if the list is empty and mocking goods in list

--- frontend/src/components/Main/ListOfGoods.tsx
+++ frontend/src/components/Main/ListOfGoods.tsx
@@ -3,7 +3,11 @@ import { themeContext } from "../../Contexts/themeContext";
 import { languageContext } from "../../Contexts/languageContext";
 
 const ListOfGoods = () => {
-  const goods = [];
+  // const goods = [];
+  const goods = [
+    { id: 1, nameOfGood: "Bread", description: "", store: "Store" },
+    { id: 2, nameOfGood: "Bottle of water", description: "5L", store: "Store" },
+  ];
   const tContext = useContext(themeContext);
   const lContext = useContext(languageContext);
 
@@ -14,20 +18,51 @@ const ListOfGoods = () => {
   const { language } = lContext;
 
   return (
-    <div className="w-full  flex flex-col flex-1 items-center justify-center">
-      {goods.length == 0 ? (
-        <>
-          <p></p>
-          <button>
+    <div className="w-full  flex flex-col flex-1 items-center px-5">
+      {goods.length == 0 && (
+        <div className=" justify-center">
+          <p className="opacity-50 ">
             {language == "eng"
               ? "There are no goods in the list."
               : language == "deu"
               ? "Es sind keine Waren in der Liste aufgeführt."
               : "W liście nie ma żadnych  towarów."}
+          </p>
+          <button className="bg-[var(--accent)] px-6 py-1 rounded-[10px] mt-3 border border-transparent hover:border-[var(--accent)] transition-colors hover:bg-transparent focus:outline focus:outline-[var(--accent)] focus:bg-transparent duration-300">
+            {language == "eng"
+              ? "+ Add to list"
+              : language == "deu"
+              ? "+ Zur Liste hinzufügen"
+              : "+ Dodać do listy"}
           </button>
+        </div>
+      )}
+      {goods.length > 0 && (
+        <>
+          <button className="ml-auto mt-4 opacity-50">
+            {language == "eng"
+              ? "+ Add to list"
+              : language == "deu"
+              ? "+ Zur Liste hinzufügen"
+              : "+ Dodać do listy"}
+          </button>
+          <ul className=" mt-4 flex flex-col gap-2.5">
+            {goods.map((g) => {
+              return (
+                <li
+                  className="h-10 w-[280px] bg-[var(--surface)] text-[var(--text)] px-2 py-1 flex justify-between "
+                  key={g.id}
+                >
+                  <div className="">
+                    <h2>{g.nameOfGood}</h2>
+                    {g.description ? <p>{g.description}</p> : <></>}
+                  </div>
+                  <p>{g.store}</p>
+                </li>
+              );
+            })}
+          </ul>
         </>
-      ) : (
-        <></>
       )}
     </div>
   );
