7f5dd706dae0457cacd9d6f4be179a694a14eaac
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-13 21:24:39 +0300

    corrected server running

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,8 @@
 import express = require("express");
 import cors = require("cors");
 import dotenv = require("dotenv");
-const pool = require("./lib/db");
+import listRoute from "./routes/list";
+import errorRoute from "./routes/error";
 
 dotenv.config();
 
@@ -11,15 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", async (req: express.Request, res: express.Response) => {
-  try {
-    const data = await pool.query("SELECT * FROM test");
-    res.status(200).json({ data: data.rows });
-  } catch (error) {
-    res.status(500).json({ message: "sth went wrong", error: error });
-    console.log(error);
-  }
-});
+app.use("/api", listRoute);
+app.use(errorRoute);
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
