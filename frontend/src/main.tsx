f33d32337a799d3dcd4bdbf7edf9d50300df74d7
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-14 23:14:51 +0300

    add query layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import Layout from "./Layout.tsx";
+import QueryLayout from "./QueryLayout.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Layout>
-      <App />
+      <QueryLayout>
+        <App />
+      </QueryLayout>
     </Layout>
   </StrictMode>
 );
