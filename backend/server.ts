cb4e5c9528758db6e3473a2e7720b4da0e21774a
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-18 01:00:21 +0300

    add user route

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express = require("express");
 import cors = require("cors");
 import dotenv = require("dotenv");
 import listRoute from "./routes/list";
+import usersRoute from "./routes/users";
 import errorRoute from "./routes/error";
 
 dotenv.config();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api", listRoute);
+app.use("/api", usersRoute);
 app.use(errorRoute);
 
 app.listen(PORT, () => {
