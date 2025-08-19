38681f6f4045e4b36209cb28aeb7b8ffeeaa81f8
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-19 14:31:42 +0300

    raw route system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { useContext } from "react";
 import { themeContext } from "./Contexts/themeContext";
 import Signup from "./Auth/Signup/Signup";
-import Header from "./components/Header/Header";
-import ListOfGoods from "./components/Main/ListOfGoods";
+import { Route, Routes } from "react-router-dom";
+import Signin from "./Auth/Signin/Signin";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const chosenTheme = useContext(themeContext);
@@ -11,9 +12,12 @@ function App() {
       className="min-h-screen w-full bg-[var(--background)] flex flex-col"
       data-theme={chosenTheme?.theme}
     >
-      {/* <Header />
-      <ListOfGoods /> */}
-      <Signup />
+      <Routes>
+        <Route loader={false} path="/signup" element={<Signup />} />
+        <Route loader={false} path="/signin" element={<Signin />} />
+        <Route loader={false} path="/" element={<Dashboard />} />
+      </Routes>
+      {/* <Signup /> */}
     </div>
   );
 }
