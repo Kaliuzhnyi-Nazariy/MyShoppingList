b8d505b126c2a9eed670f0cb84c1f83081d97636
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-19 14:31:08 +0300

    changed axios logic

--- frontend/src/Query/user.ts
+++ frontend/src/Query/user.ts
@@ -1,39 +1,42 @@
-import axios from "axios";
+import api from "./axios.util";
 import type { AddUser } from "../types";
 
 const setHeader = (token: string) => {
-  axios.defaults.headers.authoriztion = `Bearer ${token}`;
+  localStorage.setItem("token", token);
+  api.defaults.headers.authorization = `Bearer ${token}`;
 };
 
 const cleanHeader = () => {
-  axios.defaults.headers.authoriztion = ``;
+  localStorage.removeItem("token");
+  api.defaults.headers.authorization = ``;
 };
 
 export const getUser = async () => {
-  const res = await axios.get("/users");
-  return res.data.data;
+  const res = await api.get("/users");
+  console.log(res);
+  return res.data;
 };
 
 export const addUser = async (data: AddUser) => {
-  const res = await axios.post("/users/signup", data);
+  const res = await api.post("/users/signup", data);
   setHeader(res.data.token);
-  return res.data.data;
+  return res.data;
 };
 
 export const loginUser = async (data: { email: string; password: string }) => {
-  const res = await axios.post("/users/signin", data);
+  const res = await api.post("/users/signin", data);
   setHeader(res.data.token);
-  return res.data.data;
+  return res.data;
 };
 
 export const deleteUser = async (data: { id: number }) => {
-  const res = await axios.delete("/users", { data });
+  const res = await api.delete("/users", { data });
   cleanHeader();
-  return res.data.data;
+  return res.data;
 };
 
 export const logout = async () => {
-  const res = await axios.post("/users/logout");
+  const res = await api.post("/users/logout");
   cleanHeader();
-  return res.data.data;
+  return res.data;
 };
