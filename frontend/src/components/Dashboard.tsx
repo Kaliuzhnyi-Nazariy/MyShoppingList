a8da11594ec2e87b41c7bf72550940f6e622c6a9
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-19 14:30:18 +0300

    assemble list and header in one component

---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+// import ListOfGoods from "./List/ListOfGoods";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      {/* <ListOfGoods /> */}
+    </>
+  );
+};
+
+export default Dashboard;
