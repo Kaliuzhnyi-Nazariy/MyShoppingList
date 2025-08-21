156617a4bda991980f0cc28ba4569e02660fdd14
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-22 00:18:20 +0300

    add returns and commenting all consoles

--- backend/controllers/users.ts
+++ backend/controllers/users.ts
@@ -20,18 +20,25 @@ const getUser = async (
   req: Request<{}, {}, { id: number }>,
   res: Response<Answer>
 ) => {
+  // console.log("hehe");
   const { id } = (req as unknown as RequestUser).user;
+
+  // console.log({ id });
 
   const user = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
 
+  // console.log(user.rows);
+
   if (user.rows.length == 0) {
-    res.status(404).json({ message: "User not found!" });
+    return res.status(404).json({ message: "User not found!" });
   }
 
   const result = user.rows[0];
   delete result.password;
 
-  res.status(200).json(result);
+  // console.log({ result });
+
+  return res.status(200).json(result);
 };
 
 const addUser = async (
@@ -45,11 +52,11 @@ const addUser = async (
   ]);
 
   if (isUser.rows.length > 0) {
-    res.status(409).json({ message: "This email already in use!" });
+    return res.status(409).json({ message: "This email already in use!" });
   }
 
   if (password !== confirmPassword) {
-    res.status(400).json({ message: "Bad passwords!" });
+    return res.status(400).json({ message: "Bad passwords!" });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -74,7 +81,7 @@ const addUser = async (
 
   delete dataWithToken.password;
 
-  res.status(201).json({ data: dataWithToken.rows[0], token });
+  return res.status(201).json({ data: dataWithToken.rows[0], token });
 };
 
 const loginUser = async (
@@ -88,11 +95,13 @@ const loginUser = async (
   ]);
 
   if (user.rows.length == 0) {
-    res.status(400).json({ message: "Email or password is wrong!" });
+    return res.status(400).json({ message: "Email or password is wrong!" });
   }
 
-  if (password != user.rows[0].password) {
-    res.status(400).json({ message: "Email or password is wrong!" });
+  const isPasswordMatch = await bcrypt.compare(password, user.rows[0].password);
+
+  if (isPasswordMatch) {
+    return res.status(400).json({ message: "Email or password is wrong!" });
   }
 
   const result = user.rows[0];
@@ -105,38 +114,50 @@ const loginUser = async (
     expiresIn: "256h",
   });
 
-  delete result.password;
+  const dataWithToken = await pool.query(
+    "UPDATE users SET token=$1 WHERE id=$2 RETURNING *",
+    [token, result.id]
+  );
 
-  res.status(200).json({ data: result, token });
+  delete dataWithToken.password;
+
+  return res.status(200).json({ data: dataWithToken, token });
 };
 
 const deleteUser = async (req: Request, res: Response<Answer>) => {
   const { id } = (req as unknown as RequestUser).user;
-  const user = await pool.query("DELETE FROM users WHERE id = $1 RETURNING *", [
-    id,
-  ]);
+  // console.log({ id });
+  if (!id) return res.status(400).json({ message: "user not found!" });
+
+  const user = await pool.query(`DELETE FROM users WHERE id=${id} RETURNING *`);
 
   if (user.rows.length == 0) {
-    res.status(404).json({ message: "User not found" });
+    return res.status(404).json({ message: "User not found" });
   }
   const result = user.rows[0];
   delete result.password;
 
-  res.status(200).json(result);
+  return res.status(200).json(result);
 };
 
 const logout = async (req: Request, res: Response) => {
   const { id } = (req as unknown as RequestUser).user;
 
-  const data = await pool.query(`UPDATE users SET token='' WHERE id=${id}`);
+  // console.log({ id });
+
+  const data = await pool.query(
+    `UPDATE users SET token='' WHERE id=${id} RETURNING *`
+  );
+
+  // console.log(data.rows);
 
   if (data.rows.length == 0)
-    res.status(500).json({ message: "Something went wrong!" });
+    return res.status(500).json({ message: "Something went wrong!" });
 
   const result = data.rows[0];
   delete result.password;
 
-  res.status(200).json({ data: result });
+  return res.status(204).json({ data: result });
 };
 
 export default {
