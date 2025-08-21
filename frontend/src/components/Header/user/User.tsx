a12eb5f4dde721b77033b113ee78c1951765075a
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-22 00:24:37 +0300

    using react context

--- frontend/src/components/Header/user/User.tsx
+++ frontend/src/components/Header/user/User.tsx
@@ -1,17 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "../../../Query/user";
+// import { useQuery } from "@tanstack/react-query";
+// import { getUser } from "../../../Query/user";
 import UserButton from "./UserButton";
+import { useContext } from "react";
+import { userContext } from "../../../Contexts/userContext";
 
 const User = () => {
-  const { data, isPending } = useQuery({
-    queryKey: ["userdata"],
-    queryFn: getUser,
-  });
+  const uContext = useContext(userContext);
 
-  console.log(data);
+  if (!uContext)
+    return "something should work, but not work I mean reaqt query layout";
+
+  const { name } = uContext;
+
+  // const { data, isPending } = useQuery({
+  //   queryKey: ["userdata"],
+  //   queryFn: getUser,
+  // });
+
+  // console.log(data);
   return (
     <div className="relative">
-      {isPending ? "Loading" : data ? <UserButton name={data.name} /> : "Non"}
+      {<UserButton name={name || "none"} />}
+      {/* {isPending ? "Loading" : data ? <UserButton name={data.name} /> : "Non"} */}
+      {/* none */}
     </div>
   );
 };
