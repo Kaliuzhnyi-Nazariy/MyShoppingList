cfabd5158eca51c03126b3a84d07034740f8e0cf
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-24 09:41:43 +0300

    prepare to deploy

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,8 @@ import dotenv = require("dotenv");
 import listRoute from "./routes/list";
 import usersRoute from "./routes/users";
 import errorRoute from "./routes/error";
+import helmet from "helmet";
+import path = require("path");
 
 dotenv.config();
 
@@ -13,9 +15,19 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(helmet({ contentSecurityPolicy: false }));
+
 app.use("/api", listRoute);
 app.use("/api", usersRoute);
 app.use(errorRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.all("/{*any}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
