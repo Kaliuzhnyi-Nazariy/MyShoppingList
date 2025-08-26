5c97c80d853b77bcfb8891c394388a37ffd202bb
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-26 09:32:29 +0300

    removed .tsx in imports

--- frontend/src/Auth/Signin/Signin.tsx
+++ frontend/src/Auth/Signin/Signin.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import Form from "./Form.tsx";
+import Form from "./Form";
 
 const Signup = () => {
   return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import Layout from "./Layout.tsx";
-import QueryLayout from "./QueryLayout.tsx";
+import App from "./App";
+import Layout from "./Layout";
+import QueryLayout from "./QueryLayout";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
