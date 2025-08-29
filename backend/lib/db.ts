f07ec8b760670cec0198bf174fe6674a951fd9e3
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-29 14:27:44 +0300

    add user for pool

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -3,11 +3,12 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const { DB_CONNECTION_LINK } = process.env;
+const { DB_CONNECTION_LINK, DB_USER_GIVEN } = process.env;
 
 const pool = new Pool({
   connectionString: DB_CONNECTION_LINK,
   ssl: { rejectUnauthorized: false },
+  user: DB_USER_GIVEN,
 });
 
 module.exports = pool;
