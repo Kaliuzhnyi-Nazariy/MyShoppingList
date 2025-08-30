e416e5bba6749a65e6e55f59e236f2c2e18c83b8
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-08-30 06:50:12 +0300

    change link

--- frontend/src/Query/axios.util.ts
+++ frontend/src/Query/axios.util.ts
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    process.env.NODE_ENV === "production" ? "" : "http://localhost:3002/api",
+    process.env.NODE_ENV === "production"
+      ? "/api"
+      : "http://localhost:3002/api",
 });
 
 const token = localStorage.getItem("token");
