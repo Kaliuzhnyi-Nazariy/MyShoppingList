d03161f85389c79d970a5eca86812045013ad7e6
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-07 22:36:51 +0200

    another try db connection

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -6,9 +6,15 @@ dotenv.config();
 const { PG_INTERNAL, DB_USER_GIVEN } = process.env;
 
 const pool = new Pool({
-  connectionString: PG_INTERNAL,
+  // connectionString: PG_INTERNAL,
+  // ssl: { rejectUnauthorized: false },
+  // user: DB_USER_GIVEN,
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  port: Number(process.env.DB_PORT),
   ssl: { rejectUnauthorized: false },
-  user: DB_USER_GIVEN,
 });
 
 module.exports = pool;
