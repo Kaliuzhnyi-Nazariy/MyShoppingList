c8e73c886fcdf37dbf50f7dca9168462053f0667
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2025-11-09 13:53:16 +0200

    try to connect db

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -9,11 +9,19 @@ const pool = new Pool({
   // connectionString: PG_INTERNAL,
   // ssl: { rejectUnauthorized: false },
   // user: DB_USER_GIVEN,
-  host: process.env.PG_HOST,
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: Number(process.env.DB_PORT),
+  // host: process.env.PG_HOST,
+  // user: process.env.PG_USER,
+  // password: process.env.PG_PASSWORD,
+  // database: process.env.PG_DATABASE,
+  // port: Number(process.env.DB_PORT),
+
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE,
+  // port: Number(process.env.DB_PORT),
+
+  connectionString: process.env.PG_EXTERNAL,
   ssl: { rejectUnauthorized: false },
 });
 
